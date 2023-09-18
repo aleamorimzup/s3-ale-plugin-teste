4184b8bdf6b0734aa4282b1728df6d590ce97547
aleamorimzup <alexssandra.amorim@zup.com.br>
2023-09-18 09:39:13 -0300

    ajuste tf

--- templates-deploy/s3.tf
+++ templates-deploy/s3.tf
@@ -1,11 +1,15 @@
-resource "aws_s3_bucket" "{{ name }}" {
-  bucket = "{{ name }}-bucket"
-  acl    = "private"
+resource "aws_s3_bucket" "bucket_name" {
+  bucket = var.bucket_name
 }
+
+#resource "aws_s3_bucket_acl" "bucket_name" {
+#  bucket = var.bucket_name
+#  acl = "private"  
+#}
 output "aws-s3-bucket-name" {
-  value = aws_s3_bucket.{{ name }}.id
+  value = var.bucket_name
 }
 
 output "aws-s3-arn" {
-  value = aws_s3_bucket.{{ name }}.arn
+  value = var.bucket_name
 }
---
+++ templates-deploy/variable.tf
@@ -0,0 +1,7 @@
+variable "bucket_name" {
+  type = string
+}
+
+#variable "aws_s3_bucket" {
+#  type = string
+#}
