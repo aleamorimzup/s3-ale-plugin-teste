d252410c846ac5172f282d0c5fab70003672985c
aleamorimzup <alexssandra.amorim@zup.com.br>
2023-09-19 08:41:16 -0300

    ajuste variables

--- templates-deploy/s3.tf
+++ templates-deploy/s3.tf
@@ -1,12 +1,8 @@
-resource "aws_s3_bucket" "bucket_name" {
-  bucket = "bucket_name-bucket"
+resource "aws_s3_bucket" "bucket_{{ bucket_name }}" {
+bucket = var.bucket_name
   acl = "private"
 }
 
-#resource "aws_s3_bucket_acl" "bucket_name" {
-#  bucket = var.bucket_name
-#  acl = "private"  
-#}
 output "aws-s3-bucket-name" {
   value = "${aws_s3_bucket.bucket_name.id}"
 }
